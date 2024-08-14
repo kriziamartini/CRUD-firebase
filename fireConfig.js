813da3116e4083e21d4e1e6904567df255508119
kriziamartini <144977671+kriziamartini@users.noreply.github.com>
2024-08-15 03:56:48 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB777uB4TuGnektAqvZ2tMAJphzb5_8VGc",
+  authDomain: "crud-with-firebase-25f92.firebaseapp.com",
+  databaseURL: "https://crud-with-firebase-25f92-default-rtdb.firebaseio.com",
+  projectId: "crud-with-firebase-25f92",
+  storageBucket: "crud-with-firebase-25f92.appspot.com",
+  messagingSenderId: "820324956229",
+  appId: "1:820324956229:web:e14e4d14d192edfbb00b2c",
+};
+
+
+const app = initializeApp(firebaseConfig);
